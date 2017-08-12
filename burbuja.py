188f496bae0ae7bf76ed5d0777d3495d0fe38ede
franciscoojavier <30957010+franciscoojavier@users.noreply.github.com>
2017-08-12 10:17:45 -0500

    Create burbuja.py

---
+++ burbuja.py
@@ -0,0 +1,8 @@
+lista = [5,2,80,4,1,90,7,6,3,8]
+for recorrido in range (1,len(lista)) :
+for posicion in range (len(lista) - recorrido) :
+if lista [posicion] > lista [posicion + 1] :
+temp = lista [posicion]
+lista [posicion] = lista [posicion + 1]
+lista [posicion + 1] = temp
+print (lista)
